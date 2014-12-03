420a9f9c545c0f680ee7d7a2aa6b1fa7f62a1029
Lindsey Kuper <lindsey@composition.al>
2014-12-02 16:32:47 -0800

    Add callback-based version, as a starting point.

--- taskjs-ctypes/data/content.js
+++ taskjs-ctypes/data/content.js
@@ -1,1 +1,12 @@
-// TODO
+function lengthInBytes(arg, callback) {
+    console.log("arg to lengthInBytes:" + arg);
+    self.port.emit("lengthInBytesCalled", arg);
+
+    self.port.on("lengthInBytesReturned", function(result) {
+        console.log("result of lengthInBytes: " + result);
+        callback(result);
+    });
+}
+
+exportFunction(lengthInBytes, unsafeWindow, {defineAs: "lengthInBytes",
+                                             allowCallbacks: true});
--- taskjs-ctypes/lib/main.js
+++ taskjs-ctypes/lib/main.js
@@ -1,1 +1,66 @@
-// TODO
+// Get "chrome privileges" to access the Components object.
+var {Cu, Cc, Ci} = require("chrome");
+
+Cu.import("resource://gre/modules/ctypes.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+var pageMod = require("sdk/page-mod");
+var data = require("sdk/self").data;
+pageMod.PageMod({
+    include: ["*", "file://*"],
+    attachTo: ["existing", "top"],
+    contentScriptFile: data.url("content.js"),
+    contentScriptWhen: "start", // Attach the content script before any page script loads.
+
+    onAttach: function(worker) {
+        worker.port.on("lengthInBytesCalled", function(arg) {
+            console.log("arg to lengthInBytes in main.js: " + arg);
+            let result = lengthInBytes(arg);
+
+            console.log("result of lengthInBytes in main.js: " + result);
+            worker.port.emit("lengthInBytesReturned", result);
+        });
+    }
+});
+
+let libc = {
+
+    lib: null,
+
+    init: function() {
+
+        let os = Services.appinfo.OS;
+
+        if (os == "Darwin") {
+            this.lib = ctypes.open("libc.so");
+        } else if (os == "Linux") {
+            // Try letting the system look for the library in standard locations.
+            this.lib = ctypes.open("libc.so.6");
+        } else {
+            throw "Your OS " + os + " is not supported";
+        }
+
+        // Declare the `strlen` function.
+        this.strlen = this.lib.declare("strlen",
+                                       ctypes.default_abi,
+                                       ctypes.size_t,
+                                       ctypes.char.ptr);
+    },
+
+    shutdown: function() {
+        this.lib.close();
+    },
+}
+
+function lengthInBytes(str) {
+    // str is a JS string; convert it to a ctypes string.
+    let cString = ctypes.char.array()(str);
+
+    libc.init();
+    let length = libc.strlen(cString);
+    libc.shutdown();
+
+    // `length` is a ctypes.UInt64; turn it into a JSON-serializable
+    // string before returning it.
+    return length.toString();
+}
